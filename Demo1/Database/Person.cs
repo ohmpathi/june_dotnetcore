c82f781229e24fb33836fcce802396cf6e297355
Ompathi, Madana <madana.ompathi@philips.com>
2020-06-11 18:14:55 +0530

    july 9th (2)

--- Demo1/Database/Person.cs
+++ Demo1/Database/Person.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Demo1.Database
 {
+    [Table("PeopleMaster")]
     public class Person
     {
-        public int Id { get; set; }
+        [Key]
+        public int PersonNumber { get; set; }
+        
+        [MaxLength(10)]
+        [Required]
         public string Name { get; set; }
+
+        [MinLength(6)]
         public string City { get; set; }
+
+        [Column("DateOfBirth")]
         public DateTime? DOB { get; set; }
+
+        public int addressId { get; set; }
+
+        [ForeignKey("addressId")]
+        public virtual PersonAddress personAddress { get; set; }
     }
 }
